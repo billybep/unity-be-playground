c285e2e6097c37274e5d1f77c906a203f21c7eed
billyep <billypesoth@bigdade.id>
2023-01-27 12:50:56 +0800

    encrypt password, implement safe deserialization, limiting response

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 const Account = mongoose.model('accounts')
+const argon2 = require('argon2')
+const crypto = require('crypto')
+
+// const passwordRegex = new RegExp('/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})/gm')
 
 module.exports = app => {
   // Routes
@@ -9,28 +13,53 @@ module.exports = app => {
     console.log(req.body);
     const { username, password } = req.body
 
-    if (username == null || password == null) {
-      res.send("Invalid credential")
+    let response = {}
+
+    if (username == null || password.length < 3 || password.length > 24) {
+      response.code = 1
+      response.msg = 'Invalid credentials'
+      res.send(response)
+      console.log(response.msg);
       return
     }
 
-    let userAccount = await Account.findOne({ username: username })
+    let userAccount = await Account.findOne({ username: username }, 'username password')
     console.log(userAccount);
-    if (userAccount !== null) {
-      //! in prod should hash the password
-      if (password == userAccount.password) {
-        userAccount.lastAuthentication = Date.now()
-        await userAccount.save()
-  
-        console.log("Retrieving account...");
-        res.send(userAccount)
-        return
-      }
-    }
 
-    res.send("Invalid credential")
-    console.log("Invalid credential")
-    return
+    if (userAccount !== null) {
+      // verify password
+      argon2
+        .verify(userAccount.password, password)
+        .then(async success => {
+          if (!success) {
+            response.code = 1
+            response.msg = 'Invalid credentials'
+            res.send(response)
+            console.log(response.msg);
+            return
+          }
+
+          userAccount.lastAuthentication = Date.now()
+          await userAccount.save()
+
+          console.log("Retrieving account...");
+          console.log("Welcome " + userAccount.username);
+
+          response.code = 0
+          response.msg = 'Account found'
+          response.data = (({ username, _id }) => ({ username, _id }))(userAccount)
+
+          res.send(response)
+          console.log(response.data);
+          return
+        })
+    } else {
+      response.code = 1
+      response.msg = 'Invalid credentials'
+      res.send(response)
+      console.log(response.msg);
+      return
+    }
   })
 
   // ---- REGISTER
@@ -38,31 +67,64 @@ module.exports = app => {
     console.log(req.body);
     const { username, password } = req.body
 
-    if (username == null || password == null) {
-      res.send("Invalid credential")
+    let response = {}
+
+    if (username == null || username.length < 3 || username.length > 24) {
+      response.code = 1
+      response.msg = 'Invalid credentials'
+      res.send(response)
+      console.log(response.msg);
       return
     }
 
-    let userAccount = await Account.findOne({ username })
+    if (password.length < 3 || password.length > 24) {
+      response.code = 3
+      response.msg = 'Unsafe password'
+      res.send(response)
+      console.log(response.msg);
+      return
+    }
+
+    let userAccount = await Account.findOne({ username }, '_id')
+    console.log(userAccount);
 
     if (userAccount == null) {
       // Create new account
       console.log("Creating new account...");
 
-      let newAccount = new Account({
-        username,
-        password,
+      // Encrypt password
+      crypto.randomBytes(32, (err, salt) => {
 
-        lastAuthentication: Date.now()
+        if (err) console.log(err);
+
+        argon2
+          .hash(password, salt)
+          .then(async hash => {
+            let newAccount = new Account({
+              username,
+              password: hash,
+              salt,
+      
+              lastAuthentication: Date.now()
+            })
+      
+            await newAccount.save()
+
+            response.code = 0
+            response.msg = 'Account found'
+            response.data = (({ username, _id }) => ({ username }))(newAccount)
+
+            res.send(response)
+            console.log(response);
+            return
+          })
       })
-
-      await newAccount.save()
-
-      res.send(newAccount)
-      return
     } else {
-      res.send("Username has been registered")
-      console.log("User has been registered")
+      response.code = 2
+      response.msg = 'Username has been registered'
+      res.send(response)
+      console.log(response.msg);
+      return
     }
     return
   })
